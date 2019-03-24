446d0aebce920a97decb280f635ac2bc5425916c
Andre Santos <santos.as96@hotmail.com>
2019-03-24 16:20:05 +0000

    Fixed not being able to load for the second time a drawing picture on main menu

---
+++ Assets/Scripts/UIManager_MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager_MainMenu : MonoBehaviour
+{
+    private LevelManager levelManager;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void EnterGallery()
+    {
+        levelManager.LoadLevel(2);
+    }
+
+    public void SetPictureOnGameManager(GameObject g)
+    {
+        gameManager.SetPictureToDraw(g);
+    }
+
+    public void EnterGame()
+    {
+        levelManager.LoadLevel(1);
+    }
+}
