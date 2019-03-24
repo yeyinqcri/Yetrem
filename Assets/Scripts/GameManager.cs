6e75d0b3f2af562fd6accc537fa5037aa67007f1
Andre Santos <santos.as96@hotmail.com>
2019-03-24 04:12:21 +0000

    Added a list where the user can choose which picture to draw

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private Sprite imageToDraw;
+
+    private void Awake()
+    {
+
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetPictureToDraw(GameObject g)
+    {
+        imageToDraw = g.GetComponent<Image>().sprite;
+        Debug.Log("image to draw = " + imageToDraw);
+    }
+
+    public void SetNewPicture()
+    {
+        Debug.Log("h");
+        FindObjectOfType<Drawing>().gameObject.GetComponent<SpriteRenderer>().sprite = imageToDraw;
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,8 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager instance = null;
+
+    private void Awake()
+    {
+
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void LoadLevel(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex);
+    }
+
+    // called first
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            FindObjectOfType<GameManager>().SetNewPicture();
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            FindObjectOfType<GameManager>().SetNewPicture();
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
