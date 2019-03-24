c1cc71da6834cf468c605a4058d650beebf13fb5
Andre Santos <santos.as96@hotmail.com>
2019-03-24 22:55:45 +0000

    Now the user can save a picture to the gallery on the finish option and then access it and continue to paint it

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,4 +42,8 @@ public class GameManager : MonoBehaviour
     {
         return GalleryPictures;
     }
+    public void AddGalleryPicture(Sprite s)
+    {
+        GalleryPictures.Add(s);
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject PauseMenuPanel;
+    public GameObject FinishMenuPanel;
+    public Camera renderCamera;
 
     private LevelManager levelManager;
 
@@ -20,5 +23,41 @@ public class UIManager : MonoBehaviour
     public void TogglePauseMenu()
     {
         PauseMenuPanel.SetActive(!PauseMenuPanel.activeInHierarchy);
+    }
+
+    public void ToggleFinishMenu()
+    {
+        FinishMenuPanel.SetActive(!FinishMenuPanel.activeInHierarchy);
+    }
+
+    public void SaveCurrentPictureToGallery()
+    {
+        Texture2D texture = RTImage();
+        
+
+        Sprite screenshot = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height),new Vector2(0.5f,0.5f));
+
+        FindObjectOfType<GameManager>().AddGalleryPicture(screenshot);
+    }
+
+    Texture2D RTImage()
+    {
+        renderCamera.enabled = true;
+        // The Render Texture in RenderTexture.active is the one
+        // that will be read by ReadPixels.
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = renderCamera.targetTexture;
+
+        // Render the camera's view.
+        renderCamera.Render();
+
+        // Make a new texture and read the active Render Texture into it.
+        Texture2D image = new Texture2D(renderCamera.targetTexture.width, renderCamera.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, renderCamera.targetTexture.width, renderCamera.targetTexture.height), 0, 0);
+        image.Apply();
+
+        // Replace the original active Render Texture.
+        RenderTexture.active = currentRT;
+        return image;
     }
 }
