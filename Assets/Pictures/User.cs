2ac4205d927a9a7caf135122b063af2e4c1dfdea
Andre Santos <santos.as96@hotmail.com>
2019-03-24 00:56:07 +0000

    Now the user on draw mode whenever touching above the drawing

---
+++ Assets/Pictures/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    private Drawing drawing;
+
+    private void Start()
+    {
+        drawing = FindObjectOfType<Drawing>();
+    }
+    private void Update()
+    {
+        if(drawing.IsTouchOver && Input.GetButton("Fire1"))
+        {
+            Debug.Log("is drawing");
+        }
+        else
+        {
+            Debug.Log("not drawing");
+        }
+    }
+}
