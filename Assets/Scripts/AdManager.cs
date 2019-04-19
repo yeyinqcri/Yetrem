5569d6ab6ce60d120801d0b5e6a376243b4c2c1a
Andre Santos <santos.as96@hotmail.com>
2019-04-19 03:13:25 +0100

    Added Admob interstitial ads to the game, needs improvement on the timing

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,55 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    private InterstitialAd interstitial;
+    private string yetremId = "ca-app-pub-3345994290034136~3575736324";
+    private string interstitialId = "ca-app-pub-3345994290034136/1024405233";
+    private string testId = "";
+
+    private void Awake()
+    {
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+            string appId = "unexpected_platform";
+#endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+        RequestInterstitial();
+    }
+
+
+    private void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    public void ShowInterstitial()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
+}
